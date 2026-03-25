ad692df62b3e5de6d02ab350706aea336e2ac12c
Shreshth Sharma <shreshth013@gmail.com>
2026-03-25 16:59:58 +0000

    Add dex

---
+++ rig-onechain-trader/src/dex/mod.rs
@@ -0,0 +1,4 @@
+// DEX implementations using OneChain APIs will be located here
+// HTTP wrappers to Jupiter are permanently removed in favor of direct DeepBook PTB logic
+
+pub struct OneDexClient {} // Prototype stub 
